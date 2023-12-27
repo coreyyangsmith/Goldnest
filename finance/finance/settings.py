e27ecccbb4427a3735cc45ac5f02808ba65b0143
Corey Yang-Smith <yangsmith.corey@hotmail.com>
2023-12-27 07:43:20 -0700

    add environment variables

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 from datetime import timedelta
 
 from decouple import config
+from environs import Env
+
+env = Env()
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
